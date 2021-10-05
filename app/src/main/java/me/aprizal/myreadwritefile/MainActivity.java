8a2b01dfc0f4c4d8aaea948173cf319ba3b7b0a7
ApriZal <aprizalrizalzal@gmail.com>
2021-10-06 00:27:41 +0800

    Initial commit

---
+++ app/src/main/java/me/aprizal/myreadwritefile/MainActivity.java
@@ -0,0 +1,83 @@
+package me.aprizal.myreadwritefile;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private EditText editContent;
+    private EditText editTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnNew = findViewById(R.id.button_new);
+        Button btnOpen = findViewById(R.id.button_open);
+        Button btnSave = findViewById(R.id.button_save);
+
+        editTitle = findViewById(R.id.edit_title);
+        editContent = findViewById(R.id.edit_file);
+
+        btnNew.setOnClickListener(this);
+        btnOpen.setOnClickListener(this);
+        btnSave.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.button_new) {
+            newFile();
+        } else if (id == R.id.button_open) {
+            showList();
+        } else if (id == R.id.button_save) {
+            saveFile();
+        }
+    }
+
+    private void saveFile() {
+        if (editTitle.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Title harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show();
+        } else if (editContent.getText().toString().isEmpty()) {
+            Toast.makeText(this, "Kontent harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show();
+        }  else {
+            String title = editTitle.getText().toString();
+            String text = editContent.getText().toString();
+            FileModel fileModel = new FileModel();
+            fileModel.setFilename(title);
+            fileModel.setData(text);
+            FileHelper.writeToFile(fileModel, this);
+            Toast.makeText(this, "Saving " + fileModel.getFilename()  + " file", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void showList() {
+        final CharSequence[] items = fileList();
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Pilih file yang diinginkan");
+        builder.setItems(items, (dialog, item) -> loadData(items[item].toString()));
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private void loadData(String toString) {
+        FileModel fileModel = FileHelper.readFromFile(this, toString);
+        editTitle.setText(fileModel.getFilename());
+        editContent.setText(fileModel.getData());
+        Toast.makeText(this, "Loading " + fileModel.getFilename() + " data", Toast.LENGTH_SHORT).show();
+    }
+
+    private void newFile() {
+        editTitle.setText("");
+        editContent.setText("");
+        Toast.makeText(this, "Clearing file", Toast.LENGTH_SHORT).show();
+    }
+}
